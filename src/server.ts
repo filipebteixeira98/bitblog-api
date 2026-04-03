bbb9ee8195e8a964447df0fe0de7de0203b00218
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-04-03 20:56:48 -0300

    feat: configure CORS middleware for cross-origin access

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,28 @@
+import type { CorsOptions } from 'cors';
+import cors from 'cors';
 import express from 'express';
 
 import { config } from './config';
 
 const app = express();
+
+const corsOptions: CorsOptions = {
+  origin(origin, callback) {
+    if (
+      config.NODE_ENV === 'development' ||
+      !origin ||
+      config.WHITELIST_ORIGINS.includes(origin)
+    ) {
+      callback(null, true);
+    } else {
+      callback(new Error(`${origin} is not allowed by CORS`), false);
+    }
+
+    console.log(`CORS request from origin: ${origin}`);
+  },
+};
+
+app.use(cors(corsOptions));
 
 app.listen(config.PORT, () => {
   console.log(`Server running: http://localhost:${config.PORT}`);
