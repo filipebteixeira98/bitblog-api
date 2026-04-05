8ddc1845303c34c483087e8748cd3c702016e0c8
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-04-05 00:57:53 -0300

    feat: implement JWT authentication and token management

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+import type ms from 'ms';
 
 dotenv.config();
 
@@ -8,6 +9,12 @@ const config = {
   WHITELIST_ORIGINS: [''],
   MONGO_URI: process.env.MONGO_URI,
   LOG_LEVEL: process.env.LOG_LEVEL || 'info',
+  // biome-ignore lint/style/noNonNullAssertion: We are sure that these environment variables will be set, otherwise the application cannot run.
+  JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET!,
+  // biome-ignore lint/style/noNonNullAssertion: The application depends on these environment variables; without them, it will fail to initialize.
+  JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET!,
+  ACCESS_TOKEN_EXPIRY: process.env.ACCESS_TOKEN_EXPIRY as ms.StringValue,
+  REFRESH_TOKEN_EXPIRY: process.env.REFRESH_TOKEN_EXPIRY as ms.StringValue,
 };
 
 export { config };
